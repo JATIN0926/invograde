1cb6c7cd61e76fba069126b8618699f275d4d869
gauravtripathii <gauravtripathii>
2024-07-27 19:50:41 +0530

    User Model created

---
+++ src/models/userModel.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+    },
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password_hash: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    profilePicture: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    profile_title: {
+        type: String,
+    },
+    projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
+    courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+});
+
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
