7ba9d18824526919fdedde0846b91c5e068b55a7
gauravtripathii <gauravtripathii>
2024-07-30 15:14:11 +0530

    Like and Comment model created

---
+++ src/models/likeModel.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CourseVideo",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Like =
+  mongoose.models.users || mongoose.model("Likes", likeSchema);
+
+export default Like;
