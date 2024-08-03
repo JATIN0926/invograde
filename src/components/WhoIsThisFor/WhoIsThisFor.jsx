d094a4147f7ecdd919d7654ad745660efb6fafa9
Jatin Chaudhary <09jatin2005@gmail.com>
2024-08-03 23:32:35 +0530

    video player animation

--- src/components/WhoIsThisFor/WhoIsThisFor.jsx
+++ src/components/WhoIsThisFor/WhoIsThisFor.jsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { InfiniteMovingCards } from "./MovingCards";
+import { motion, useAnimationControls, useInView } from "framer-motion";
+
 const items = [
   { src: "/images/gameDev.png", text: "Game Developers", bgColor: "#3A3084" },
   {
@@ -19,7 +23,31 @@ const items = [
   { src: "/images/Appdev.png", text: "App Developers", bgColor: "#3A3084" },
   { src: "/images/tutor.png", text: "Tutors/Educators", bgColor: "#774FCC" },
 ];
+
 const WhoIsThisFor = () => {
+  const controls = useAnimationControls();
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start({
+        scale: [0, 1],
+        transition: { duration: 1, ease: "easeInOut" },
+      }).then(() => {
+        controls.start({
+          y: ["0%", "-150%"],
+          transition: { duration: 1, ease: "easeInOut" },
+        }).then(() => {
+          controls.start({
+            opacity: [1, 0],
+            transition: { duration: 1, ease: "easeInOut" },
+          });
+        });
+      });
+    }
+  }, [isInView, controls]);
+
   return (
     <>
       <div className="w-full">
@@ -41,7 +69,7 @@ const WhoIsThisFor = () => {
                   width={25}
                   height={20}
                 />
-                <h2 className=" font-IBMPlexSans-Regular">Contact Us</h2>
+                <h2 className="font-IBMPlexSans-Regular">Contact Us</h2>
               </div>
             </div>
           </div>
@@ -52,15 +80,15 @@ const WhoIsThisFor = () => {
         <div className="w-[95%] relative rounded-lg bg-[#DDCCFF] p-6 flex items-center justify-center h-[95%]">
           <div className="flex flex-col items-center justify-center gap-6 w-full">
             <div className="flex w-full ">
-              <h1 className=" font-Snippet-Regular mr-auto font-bold self-center text-[1.1rem]">
+              <h1 className="font-Snippet-Regular mr-auto font-bold self-center text-[1.1rem]">
                 invograde
               </h1>
-              <div className="flex items-center align-top justify-between w-[20%] ">
-                <h2 className=" font-IBMPlexSans-Regular text-[#141520] font-semibold">
+              <div className="flex items-center align-top justify-between w-[20%]">
+                <h2 className="font-IBMPlexSans-Regular text-[#141520] font-semibold">
                   Company
                 </h2>
-                <div className=" w-2 h-2 rounded-full bg-black"></div>
-                <h2 className=" font-IBMPlexSans-Regular text-[#141520] font-semibold">
+                <div className="w-2 h-2 rounded-full bg-black"></div>
+                <h2 className="font-IBMPlexSans-Regular text-[#141520] font-semibold">
                   About Us
                 </h2>
               </div>
@@ -68,12 +96,12 @@ const WhoIsThisFor = () => {
                 <h1 className="font-semibold font-IBMPlexSans-Regular uppercase">
                   Get a Demo
                 </h1>
-                <div className=" relative w-7 h-10">
+                <div className="relative w-7 h-10">
                   <Image src="/icons/arrow-up.svg" alt="img" fill />
                 </div>
               </div>
             </div>
-            <p className=" font-IBMPlexSans-SemiBold text-[2.5rem] leading-[3rem] text-center w-[90%]">
+            <p className="font-IBMPlexSans-SemiBold text-[2.5rem] leading-[3rem] text-center w-[90%]">
               Connect, inspire, and collaborate with fellow creatives and
               professionals across various industries. Through showcasing your
               creative projects and achievements, you aim to seek opportunities
@@ -83,19 +111,28 @@ const WhoIsThisFor = () => {
               success. Let us link, inspire, and create together!
             </p>
           </div>
-          <div className=" absolute left-[50%] top-[52%] translate-x-[-50%] translate-y-[-50%] bg-white w-[50%] aspect-video rounded-3xl">
+          <motion.div
+            ref={ref}
+            initial={{ scale: 0, y: "0%", opacity: 1 }}
+            animate={controls}
+            className="absolute translate-x-[-50%] translate-y-[-50%] bg-white w-[50%] aspect-video rounded-3xl"
+          >
             <div className="w-full h-full relative">
-              <div className=" absolute left-[5%] bottom-[8%] bg-[#D9D9D9] rounded-3xl flex p-1 px-4 cursor-pointer">
+              <div className="absolute left-[5%] bottom-[8%] bg-[#D9D9D9] rounded-3xl flex p-1 px-4 cursor-pointer">
                 <div className="h-full w-full flex items-center justify-center gap-3">
-                   <h1 className=" font-IBMPlexSans-Regular font-semibold">Play</h1>
-                   <div className="bg-[#141520] h-5 w-5 rounded-full flex items-center justify-center">
-                    <Image src="/icons/play_btn_arrow.svg" alt="img" width={8} height={10} />
-                   </div>
+                  <h1 className="font-IBMPlexSans-Regular font-semibold">Play</h1>
+                  <div className="bg-[#141520] h-5 w-5 rounded-full flex items-center justify-center">
+                    <Image
+                      src="/icons/play_btn_arrow.svg"
+                      alt="img"
+                      width={8}
+                      height={10}
+                    />
+                  </div>
                 </div>
-                
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
